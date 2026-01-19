5fc95e39bb9721a02c73a06ce8014655f54cfb0c
Nathan <nathanmacharia115@gmail.com>
2026-01-19 16:49:02 +0300

    Add database models and startup table creation logic

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from typing import Union
 
 from fastapi import FastAPI
+from models import Base,engine
 
 app = FastAPI()
+
+# Create tables on startup
+@app.on_event("startup")
+def create_tables():
+    Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
 def read_root():
     return {"Duka FastAPI": "Version 1.0"}
+
